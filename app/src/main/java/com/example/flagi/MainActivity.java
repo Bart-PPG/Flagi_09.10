ff4baa0e5c283700fcbf053eff94c22977f24640
Bart-PPG <bartek.nitek@onet.pl>
2024-10-09 12:38:57 +0200

    obsluzono pytanie

--- app/src/main/java/com/example/flagi/MainActivity.java
+++ app/src/main/java/com/example/flagi/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.flagi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+private int licznik = 0;
+private TextView textView;
+private int pomylki = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void ZlaOdpowiedz(View view) {
+        Toast.makeText(this, "Ten kolor jest w fladze polski", Toast.LENGTH_SHORT).show();
+        pomylki++;
+    }
+
+    public void DobraOdpowiedz(View view) {
+        view.setVisibility(View.INVISIBLE);
+        licznik++;
+        if(licznik == 4){
+            textView.setText("To jest flaga polski \n Pomyłki = " + pomylki);
+        }
     }
 }
